7ce8cb915d569a5e754e8dffbcd8421c58368a17
joseD <josed3000@gmail.com>
2023-01-11 17:24:02 +0100

    browserRoute mistake solved

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import './App.css';
 
 function App() {
   return (
-   <BrowserRouter>
+   <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/contact" element={<Contact />} />
